4d52697bb5de696db3528555e9302be5e7dbbf6c
Paul Moine <pablo_mo@orange.fr>
2018-10-14 13:22:18 +0000

    Mettre à jour DatabaseLinks/dbl_default.php

--- DatabaseLinks/dbl_default.php
+++ DatabaseLinks/dbl_default.php
@@ -25,6 +25,7 @@
 
 	function insertNewPlayer ($nickname, $surname, $name, $mail, $pwd) {
 		global $db;
+		echo $pwd;
 		$verif=false;
 		$q = $db->prepare('SELECT nickname
                          		FROM Player');
@@ -39,6 +40,7 @@
 		if ($verif === false) {
 			$q->closeCursor();
 			$q = $db->prepare("INSERT INTO Player(nickname, surname, name, mail, pwd) VALUES (?, ?, ?, ?, ?)");
+			echo $pwd;
 			$q->execute([htmlspecialchars($nickname),htmlspecialchars($surname),htmlspecialchars($name),htmlspecialchars($mail),$pwd]);
 		}
 	}
