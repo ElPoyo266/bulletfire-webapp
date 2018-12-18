5f6aaa074e962015722286928b862241c3fc872d
Paul Moine <pablo_mo@orange.fr>
2018-12-18 10:20:59 +0000

    Style du header

--- Templates/Global/header.php
+++ Templates/Global/header.php
@@ -3,6 +3,7 @@
     <ul>
 
         <a href="<?= PATH . '/actu'?>"><li>Actualités</li></a>
+        <a href="<?= PATH . '/forum'?>"><li>Forum</li></a>
         <a href="<?= PATH . '/shop'?>"><li>Boutique</li></a>
         <?php if(isset($_SESSION["Player"])) : ?>
         <a href="<?= PATH . '/shop/basket'?>"><li>Panier</li></a>
