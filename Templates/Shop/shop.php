25dcf309ab4e54d2a01f364d11c9a840b1090501
Paul Moine <pablo_mo@orange.fr>
2018-12-18 10:42:44 +0000

    Ajout css pour le shop

--- Templates/Shop/shop.php
+++ Templates/Shop/shop.php
@@ -7,7 +7,7 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <title>Bulletfire - Boutique</title>
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="stylesheet" type="text/css" media="screen" href="<?= ASSET . 'CSS/shopHome.css'?>" />
+        <link rel="stylesheet" type="text/css" media="screen" href="<?= ASSET . 'CSS/shop.css'?>" />
         <script src="<?= ASSET . 'JS/shop.js'?>"></script>
         <script src="https://ajax.aspnetcdn.com/ajax/jQuery/jquery-3.3.1.min.js"></script>
     
@@ -20,8 +20,8 @@
         ?>
             <div>
                 <h2><?= $p->getName()?></h2>
-                <h3><?= $p->getDesc()?></h3>
                 <img src="<?= $p->getPic()?>" alt="Pic">
+                <h3><?= $p->getDesc()?></h3>
                 <strong>Prix : <?= $p->getPrice()?></strong>
                 <?php if(isset($_SESSION["Player"])) : ?>
                     <button type="button" onclick="addInBasket(<?= $p->getId() ?>)">Ajouter au panier</button>
