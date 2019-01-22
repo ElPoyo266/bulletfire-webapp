f178cd2d68a3c558f3fb48acf28e3fbc2a49288e
Alec <alec.narayanin-richenapin@etu.univ-lyon1.fr>
2019-01-22 20:47:11 +0100

    Integration css au commentaire (corr)

--- _config/hide/template.php
+++ _config/hide/template.php
@@ -6,10 +6,10 @@
 		private $content;
 
 		public function __construct ($view, $param = null) {
-			define("ASSET", "/web/Assets/");
-			define("PATH", "/web/");
-			//define("ASSET", "/Assets/");
-			//define("PATH", "");
+			//define("ASSET", "/web/Assets/");
+			//define("PATH", "/web/");
+			define("ASSET", "/Assets/");
+			define("PATH", "");
 			require "_config/hide/translation.php";
 			$translate = new Translation();
 			$filePath = "Templates/" . explode(":",$view)[0] . "/" . explode(":", $view)[1] . ".php";
