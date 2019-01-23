cc478a503e6bef25f93177ca4ef40db039025f82
Polo <pablo_mo@orange.fr>
2019-01-23 21:48:49 +0100

    Debug

--- _config/hide/template.php
+++ _config/hide/template.php
@@ -6,10 +6,10 @@
 		private $content;
 
 		public function __construct ($view, $param = null) {
-			define("ASSET", "/BulletFire/Assets/");
-			define("PATH", "/BulletFire/");
-			//define("ASSET", "/Assets/");
-			//define("PATH", "");
+			//define("ASSET", "/BulletFire/Assets/");
+			//define("PATH", "/BulletFire/");
+			define("ASSET", "/Assets/");
+			define("PATH", "");
 			require "_config/hide/translation.php";
 			$translate = new Translation();
 			$filePath = "Templates/" . explode(":",$view)[0] . "/" . explode(":", $view)[1] . ".php";
