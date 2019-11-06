20a9e6fe474600e19d42de6b1d5a47d19d9e4ff0
Polo <pablo_mo@orange.fr>
2019-11-06 17:14:00 +0100

    Change some things

--- index.php
+++ index.php
@@ -1,25 +1,29 @@
-<?php 
-	
-	// User Entity (for connection)
-	require "Classes/Entities/player.php";
-	require "Classes/Other/Shop/basket.php";
-	require "Classes/Other/Shop/product.php";
-	// End
+<?php
 
-	session_start();
+// User Entity (for connection)
+require "Classes/Entities/player.php";
+require "Classes/Other/Shop/basket.php";
+require "Classes/Other/Shop/product.php";
+// End
 
-	// Config
-	require "_config/hide/route.php";
-	require "_config/hide/template.php";
-	require "_config/hide/controller.php";
-	require "_config/hide/services.php";
-	require "_config/hide/dblink.php";
-	require "_config/hide/config.php";
-	// End
+session_start();
 
-	// Your script(s) to interact with the database(s)
-	require "_config/connectdb.php";
-	// End
+// Config
+require "_config/hide/route.php";
+require "_config/hide/template.php";
+require "_config/hide/controller.php";
+require "_config/hide/services.php";
+require "_config/hide/dblink.php";
+require "_config/hide/config.php";
+// End
 
-	$routes->submit();
-?>
+require "Config/global.php";
+
+// Your script(s) to interact with the database(s)
+require "_config/connectdb.php";
+// End
+
+$routes->submit();
+
+
+git filter-branch --force --index-filter 'git rm --cached --ignore-unmatch Assets/Pics/demande_convention.pdf' --prune-empty --tag-name-filter cat -- --all
